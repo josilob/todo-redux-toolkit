88185ce91946914b3553b5a08196032a0c9e896f
josilob <bjosilo@gmail.com>
2021-05-18 22:52:41 -0400

    basic setup, pre-redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='app'>
+			<h1>Todo App</h1>
+			<Input />
+		</div>
+	);
 }
 
 export default App;
