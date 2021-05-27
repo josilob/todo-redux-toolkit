db9de5d0a304442c03136796af6288b000fd2f8a
josilob <bjosilo@gmail.com>
2021-05-27 19:02:39 -0400

    slice setup w/ reducer and actions

---
+++ src/components/redux/todoSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+	name: 'todos',
+	initialState: [
+		{ id: 1, title: 'todo 1', completed: false },
+		{ id: 2, title: 'todo 2', completed: false },
+		{ id: 3, title: 'todo 3', completed: true }
+	],
+	reducers: {
+		addTodo: (state, action) => {
+			const newTodo = {
+				id: Date.now(),
+				title: action.payload.title,
+				completed: false
+			};
+			state.push(newTodo);
+		}
+	}
+});
+
+// export actions
+export const { addTodo } = todoSlice.actions;
+
+// export reducer
+export default todoSlice.reducer;
