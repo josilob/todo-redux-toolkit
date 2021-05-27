4b9a947f52a9f8ab285af7b3d9abde30d5179f0a
josilob <bjosilo@gmail.com>
2021-05-27 18:46:28 -0400

    store created and connected to the app

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../features/todoSlice';
 
 export default configureStore({
-	reducer: { todos: todoReducer }
+	reducer: {}
 });
